5ea1410dfae4f5660570cf6e7f3946305ea5c215
Jiyong <56155855+wldyd920@users.noreply.github.com>
2024-10-10 15:24:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+from flask import render_template, Flask, redirect, request
 import pandas as pd
+
+
+@app.route('/')
+def home():
+    return render_template('1.html')
+
+app.run(port=5000, debug=True)
